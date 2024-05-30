089434afea2f60d633936fcf5f2c8c180fdc1051
BrianOdero <odero7537@gmail.com>
2024-05-30 18:38:44 +0300

    implementing login

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php 
-$success=0;
-$user=0;
+
+
+$login=0;
+$invalid=1;
 
 
 
@@ -11,24 +13,20 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $username=$_POST['username'];
     $password=$_POST['password'];
 
+    //CONFIRMING IF USERNAME AND PASSWORD BOTH MATCH WITH THE REQUIRED CREDENTIALS
+    $sql="select * from `registration` where username='$username' and password='$password' ";
 
-    $sql="select * from `registration` where username='$username'";
     $result = mysqli_query($con,$sql);
     if($result){
         //WILL COUNT THE NUMBER OF ROWS IN OUR DATABASE AND CONFIRMS IF ONLY ONE INSTANCE OF A USERNAME IS PRESENT
         $num=mysqli_num_rows($result);
         if($num==1){
-            //echo "User Already Exists";
-            $user=1;
+            $login=1;
+            session_start();
+            $_SESSION['username']=$username; 
+            header('location:home.php');
         }else{
-            $sql="INSERT INTO `registration`(username,password) values('$username','$password ')"; 
-            $result=mysqli_query($con,$sql);
-            if($result){
-                //echo "Sign Up Successful";
-                $success=1;
-            }else{
-                die(mysqli_error($con));
-            }
+            $invalid=1;
         }
      }
 }
@@ -43,40 +41,41 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SignUp page</title>
+    <title>Login page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
   </body>
 
 <?php
-//CODE WILL EXECUTE IF USER EXISTS
-if($user){
+//CODE THAT WILL CHECK IF USER IS LOGGED IN
+if($invalid){
      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-     <strong>Uhh Ooh!</strong> This user already exists
+     <strong>Uhh Ooh!</strong> Wrong credentials
      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
    </div>';
 }
 ?>
 
 <?php
-//CODE WILL EXECUTE IF USER HAS SIGNED UP SUCCESSFULLY
-if($success){
+//CODE WILL EXECUTE IF USER HAS LOGGED IN SUCCESSFULLY
+if($login){
      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-     <strong>Hurray!! </strong> You have successfully siged up
+     <strong>Hurray!! </strong> You have successfully logged in
      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
    </div>';
 }
 ?>
 
+
     
   
   
   
-  <h1 class="text-center" >Sign Up Page</h1>
+  <h1 class="text-center" >Login Page</h1>
 
   <div class="container mt-5">
-  <form action="sign.php" method="post">
+  <form action="login.php" method="post">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Name</label>
     <input type="text" class="form-control" placeholder="Enter your username" name="username">
@@ -89,7 +88,7 @@ if($success){
   <div class="mb-3 form-check">
     
   </div>
-  <button type="submit" class="btn btn-primary w-100">Sign Up</button>
+  <button type="submit" class="btn btn-success w-100">login</button>
 </form>
   </div>
 </html>
