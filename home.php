eff6918c17e650f5f0ca59d36ba7cb9f543cbb1a
BrianOdero <odero7537@gmail.com>
2024-05-31 10:43:03 +0300

    corrected navigation using header instead of include

--- home.php
+++ home.php
@@ -19,7 +19,7 @@ if(!isset($_SESSION['username'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Welcome to the home page</h1>
+    <h3>Welcome to the home page, <?php  echo $_SESSION['username']; ?></h3>
     
   </body>
 </html>
--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             $login=1;
             session_start();
             $_SESSION['username']=$username;
-            include('home.php');
+            header('location:home.php');
         }else{
            $invalid= 1;
         }
